00963746d30b2e8ac4e739bf71ae0a2d0689deb6
Andy Golay <andygolay@gmail.com>
2025-06-05 15:39:36 -0400

    test: attempt to use core resources account, still get Connection refused (os error 61)

--- checks/migrator/checks/pre-l1-merge/src/pre_l1_merge.rs
+++ checks/migrator/checks/pre-l1-merge/src/pre_l1_merge.rs
@@ -2,11 +2,13 @@
 pub mod test {
 
 	use anyhow::Context;
+	use mtma_node_test_types::criterion::movement_executor::maptos_opt_executor::aptos_types::account_address::AccountAddress;
 	use mtma_migrator_pre_l1_merge_core::config::Config as PreL1MergeConfig;
 	use mtma_migrator_test_accounts_equal_criterion::AccountsEqual;
 	use mtma_migrator_test_types::check::checked_migration;
 	use mtma_migrator_types::migrator::{movement_migrator::Overlays, MovementMigrator};
 	use mtma_node_test_types::prelude::Prelude;
+	use std::str::FromStr;
 	use tracing::info;
 
 	#[tokio::test(flavor = "multi_thread")]
@@ -26,16 +28,38 @@ pub mod test {
 				Ok::<_, anyhow::Error>(())
 			});
 
+			// Wait for the Celestia light node to be ready
+			tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
 			// wait for the rest client to be ready
 			// once we have this, there should also be a config, so we can then kill off the migrator and proceed
 			movement_migrator
-			.wait_for_rest_client_ready(tokio::time::Duration::from_secs(600)) // we wait for up to ten minutes because the nix flake in .vendors/movementcan be a bit slow the first time
-			.await
-			.context(
-				"failed to wait for movement migrator rest client while running accounts equal manual prelude",
-			)?;
+				.wait_for_rest_client_ready(tokio::time::Duration::from_secs(600)) // we wait for up to ten minutes because the nix flake in .vendors/movementcan be a bit slow the first time
+				.await
+				.context(
+					"failed to wait for movement migrator rest client while running accounts equal manual prelude",
+				)?;
 
-			kestrel::end!(movement_migrator_task)?;
+			// Wait for the account to be created
+			let rest_client = movement_migrator
+				.wait_for_rest_client_ready(tokio::time::Duration::from_secs(30))
+				.await
+				.context("failed to get rest client")?;
+			let mut retries = 0;
+			while retries < 10 {
+				match rest_client
+					.get_account(AccountAddress::from_str(
+						"0xf90391c81027f03cdea491ed8b36ffaced26b6df208a9b569e5baf2590eb9b16",
+					)?)
+					.await
+				{
+					Ok(_) => break,
+					Err(_) => {
+						tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+						retries += 1;
+					}
+				}
+			}
 
 			// Form the prelude.
 			// todo: this needs to be updated to use the prelude generator
@@ -63,6 +87,9 @@ pub mod test {
 				}
 			}
 			info!("Migration succeeded");
+
+			// Let the task run in the background and let the Drop implementation handle the shutdown
+			drop(movement_migrator_task);
 		}
 
 		// exit the test is fine when you only have one test per crate because when cargo test is run across a workspace, it actually multi-processes the tests by crate
